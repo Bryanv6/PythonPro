29819a2c91585167379f14b75606e802dc486345
Loganv14 <lav14b@my.fsu.edu>
2017-04-19 09:26:54 -0400

    Added to basic flask structure
    Added to file structure to begin supporting the flask backend.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
 
 p_app = Flask(__name__)
+p_app.config.from_object('config')
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from app import p_app
 
 @p_app.route('/')
 @p_app.route('/index')
 def index():
-    return "Welcome to the main page!"
-    #TODO: display main page contexts here
+    return render_template('index.html')
+    #TODO: display main page contexts here.
