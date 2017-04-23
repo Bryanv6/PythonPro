2b411037210468815b53e5eb50bf156d4d12cbb6
gqm13 <gqm13@my.fsu.edu>
2017-04-23 18:10:22 -0400

    changed password fields

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
 class Register(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
-    password2 = StringField('Re-type Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Re-type Password', validators=[DataRequired()])
 
 class Login(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
 
 class Contact(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
